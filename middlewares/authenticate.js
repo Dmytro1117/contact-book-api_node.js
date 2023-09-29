99b9d775f486eb9449966c171b6213dda09dc4a9
Dmytro <moiseenkodmitriy@i.ua>
2023-09-29 23:09:34 +0300

    task-4

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,14 +7,14 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
-    throw new Unauthorized(401, `Not authorized`);
+    next(Unauthorized(401, `Not authorized`));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
 
     if (!user || !user.token || user.token !== token) {
-      throw new Unauthorized(401, `Not authorized`);
+      next(Unauthorized(401, `Not authorized`));
     }
     req.user = user;
     next();
