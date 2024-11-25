ebf90bceac070292cc03232e5f7408fc8016ba40
Dmytro <moiseenkodmitriy@i.ua>
2024-11-25 16:22:18 +0200

    task-2

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({
-    status: "error",
+    status: "fail",
     code: 500,
     message,
     data: "Internal Server Error",
