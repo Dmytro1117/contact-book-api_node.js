96dcfa602e0a47e3563e58ab10e2e21a095f4183
Dmytro <moiseenkodmitriy@i.ua>
2024-12-04 01:49:20 +0200

    task-6

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.set("strictQuery", true);
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
